9c7108662b162e32c8450e9aea227231c6072375
nick3499 <ntrinen@yahoo.com>
2020-03-18 18:44:00 -0500

    Revised code
    Revised code to satisfy `pylint` constraints for method naming conventions. See error message below:
    `Function name "rand_AZ" doesn't conform to snake_case naming style (invalid-name)`

--- random_hostname_alt1.py
+++ random_hostname_alt1.py
@@ -4,21 +4,21 @@ which generates a pseudo-random hostname.'''
 from random import randrange
 from subprocess import run
 
-def rand_az():
+def rand_lwr():
     '''Return random character: a...z'''
     return chr(randrange(97, 123, 1))
 
-def rand_AZ():
+def rand_upr():
     '''Return random character: A...Z'''
     return chr(randrange(65, 91, 1))
 
-def rand_09():
+def rand_num():
     '''Return random character: 0...9'''
     return chr(randrange(48, 58, 1))
 
 def rand_hostname():
     '''Set psuedo-random system hostname.'''
-    rand_host = f'{rand_az()}{rand_AZ()}{rand_09()}{rand_az()}{rand_AZ()}{rand_09()}'
+    rand_host = f'{rand_lwr()}{rand_upr()}{rand_num()}{rand_lwr()}{rand_upr()}{rand_num()}'
     run(['sudo', 'hostnamectl', 'set-hostname', rand_host], check=True)
 
 if __name__ == '__main__':
